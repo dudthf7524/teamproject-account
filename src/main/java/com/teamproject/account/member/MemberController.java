df887b72cf516fdd55868a397fa6c7f7290dbd7d
hdw <gom895@naver.com>
2024-08-09 23:26:46 +0900

    구글로그인 기능구현 진행중 테스트 바람

--- src/main/java/com/teamproject/account/member/MemberController.java
+++ src/main/java/com/teamproject/account/member/MemberController.java
@@ -1,9 +1,12 @@
 package com.teamproject.account.member;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +19,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member/join")
-    public String join() {
+    public String join(Authentication auth) {
         return "member/join";
     }
+
     @PostMapping("/member/joinProc")
     @ResponseBody
     public ResponseEntity<?> join(
@@ -42,8 +46,18 @@ public class MemberController {
     public void logout(Authentication auth){
 
     }
-
-
+    @GetMapping("/home")
+    @ResponseBody
+    public String home(@AuthenticationPrincipal OAuth2User principal, HttpServletRequest request) {
+        if (principal != null) {
+            String name = principal.getAttribute("name");
+            String email = principal.getAttribute("email");
+            // Add additional logic here to handle user information
+            System.out.println("Name: " + name);
+            System.out.println("Email: " + email);
+        }
+        return "name: "+principal.getAttribute("name");
+    }
 
 
 }
