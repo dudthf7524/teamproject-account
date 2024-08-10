9ecc5ea3db72343bf488cd99ecdc8b664e66d128
dudthf7524 <148530411+dudthf7524@users.noreply.github.com>
2024-08-10 14:49:08 +0900

    Revert "Income"

--- src/main/java/com/teamproject/account/member/MemberRepository.java
+++ src/main/java/com/teamproject/account/member/MemberRepository.java
@@ -1,11 +1,4 @@
 package com.teamproject.account.member;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Optional<Member> findByUsername(String username);
-    Optional<Member> findByEmail(String email);
-
+public interface MemberRepository {
 }
