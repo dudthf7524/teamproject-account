c8743c7deca52a977c376472607014f0f230e818
user <dudthf7524>
2024-08-10 22:48:22 +0900

    commit

--- src/main/java/com/teamproject/account/income/IncomeController.java
+++ src/main/java/com/teamproject/account/income/IncomeController.java
@@ -57,4 +57,46 @@ public class IncomeController {
         model.addAttribute("incomeDTOList", incomeDTOList);
         return "/income/list";
     }
+
+    @GetMapping("/income/update/{incomeId}")
+    public String update(@ModelAttribute IncomeDTO incomeDTO, Model model){
+        System.out.println("incomeDTO = " + incomeDTO);
+        System.out.println("incomeDTO = " + incomeDTO.getIncomeId());
+        LocalDate today = LocalDate.now();
+        String formattedDate = today.format(DateTimeFormatter.ofPattern("yyyy-MM"));
+
+        List<IncomeDTO> incomeDTOList = incomeService.findAllByregDtContains(formattedDate);
+        model.addAttribute("incomeDTOList", incomeDTOList);
+        model.addAttribute("incomeId", incomeDTO.getIncomeId());
+        return "/income/update";
+    }
+
+    @PostMapping("/income/update")
+    public String updateForm(@ModelAttribute IncomeDTO incomeDTO, Authentication auth){
+        System.out.println("incomeDTO : " + incomeDTO );
+        MyUserDetailsService.CustomUser result = (MyUserDetailsService.CustomUser) auth.getPrincipal();
+        System.out.println(result.memberNo);
+        incomeDTO.setMemberNo(result.memberNo);
+        incomeService.save(incomeDTO);
+
+        System.out.println("accountDTO = " + incomeDTO);
+
+        incomeService.updateForm(incomeDTO);
+        return "redirect:/income/list";
+    }
+
+    @GetMapping("/income/delete/{incomeId}")
+    public String delete(@ModelAttribute IncomeDTO incomeDTO){
+        incomeService.deleteById(incomeDTO.getIncomeId());
+        return "redirect:/income/list";
+    }
+
+    @PostMapping("/income/search")
+    public String search(Model model, @ModelAttribute IncomeDTO incomeDTO){
+        System.out.println("incomeDTO = " + incomeDTO.getRegDt());
+        List<IncomeDTO> incomeDTOList = incomeService.findAllByregDtContains(incomeDTO.getRegDt());
+        model.addAttribute("incomeDTOList", incomeDTOList);
+        model.addAttribute("searchedDate", incomeDTO.getRegDt());
+        return "/income/list";
+    }
 }
