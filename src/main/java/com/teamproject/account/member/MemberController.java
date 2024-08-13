5e87203d2f1a88ec490825c41d0f5d96643a2146
hdw <gom895@naver.com>
2024-08-13 23:16:30 +0900

    회원가입시 이메일인증 기능 구현완료

--- src/main/java/com/teamproject/account/member/MemberController.java
+++ src/main/java/com/teamproject/account/member/MemberController.java
@@ -2,6 +2,7 @@ package com.teamproject.account.member;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,13 +14,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.SecureRandom;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final EmailTokenRepository emailTokenRepository;
 
     @GetMapping("/member/join")
     public String join(Authentication auth) {
@@ -41,6 +45,42 @@ public class MemberController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", e.getMessage()));
         }
     }
+
+    @PostMapping("/member/email-verify")
+    public ResponseEntity<?> verifyEmail(@RequestParam String email) throws Exception {
+        try{
+            memberService.emailCheck(email); // 이미 존재하는 이메일인지 확인
+        }catch (ValidationException e){
+            return ResponseEntity.badRequest().body(e.getErrors());
+        }
+
+        // 이메일 인증 토큰 생성 및 발송
+        String token = generateAlphaNumericToken().toString();
+        memberService.sendVerificationEmail(email, token);
+        System.out.println("이메일토큰값: "+token);
+        // 토큰 저장
+        EmailToken emailToken = new EmailToken(token, email);
+        emailTokenRepository.save(emailToken);
+
+        return ResponseEntity.ok(Map.of("message", "이메일 인증 링크가 발송되었습니다.","token", token));
+    }
+
+    //이메일 토큰6자리 랜덤생성메소드
+    public String generateAlphaNumericToken() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder token = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(characters.length());
+            token.append(characters.charAt(index));
+        }
+
+        return token.toString();
+    }
+
+
+
     @GetMapping("/login")
     public String login() {
         return "member/login";
@@ -55,8 +95,5 @@ public class MemberController {
 
         return "member/mypage.html";
     }
-
-
-
 
 }
